9e6aa861b9fb4609bad7c2b5e5c81d70228c6e24
nidom <nidom@qq.com>
2025-09-12 02:36:07 +0800

    ...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "sign_tools",
     script: "dist/main.js",
     instances: "1",
-    exec_mode: "single",
+    exec_mode: "fork",
     max_memory_restart: "6G", // 增加到 4GB
     log_rotate: true,        // 启用日志轮转
     max_size: "2M",         // 单个日志文件最大大小
