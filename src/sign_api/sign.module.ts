e830b8823677ab0b21cfd439bab1c8fedb8be04b
nidom <nidom@qq.com>
2025-09-12 02:19:58 +0800

    ...

--- src/sign_api/sign.module.ts
+++ src/sign_api/sign.module.ts
@@ -6,8 +6,9 @@ import { Module } from '@nestjs/common';
 import { TypeModule } from 'src/entitis/typeModule';
 import { SignController } from './sign.controller';
 import { UDIDMonitorService } from './udid_monitor.service';
+import { ActionModule } from 'src/actions/action.module';
 @Module({
-    imports: [TypeModule],
+    imports: [TypeModule,ActionModule],
     controllers: [SignController],
     providers: [UDIDMonitorService],
 })
