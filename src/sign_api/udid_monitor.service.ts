d76cfb756abda88ee5764f365f23eba49be79252
nidom <nidom@qq.com>
2025-09-13 21:48:25 +0800

    ..

--- src/sign_api/udid_monitor.service.ts
+++ src/sign_api/udid_monitor.service.ts
@@ -11,6 +11,7 @@ import { isEmpty } from 'class-validator';
 import { LogService } from 'src/actions/log.service';
 import { Inject } from '@nestjs/common';
 const fs = require('fs');
+const provisioning = require('provisioning');
 
 
 @Injectable()
@@ -50,93 +51,68 @@ export class UDIDMonitorService {
 
       if(isEmpty(record.cert_iss)){
 
-        await this.udid_warning(record.id);
+        await this.udid_check(record.udid);
       }
     }
+
     // const latestRecords = await this.superUDIDRepository.find({
 
     //     order: { id: 'DESC' }
     // });
 
-    // const count = await this.superUDIDRepository.count();
 
-    // if (!latestRecords) {
-    //     return { code: -1, message: '未找到对应数据' };
-    // }
-    // return latestRecords[0];
-    // let record = latestRecords[0];
-    // if(isEmpty(record.cert_iss)){
-
-    //   //5s后重试
-
-    //   const nextRecord = await this.superUDIDRepository.findOne({
-    //     where: { id: record.id },
-
-    //   });
-      
-    //   if(isEmpty(nextRecord.cert_iss)){
-
-    //     //进行预警
-    //     return { code: -1, message: '未找到对应数据' };
-    //   }
-
-
-
-    // }
-
-
-    // let file_path = `/www/wwwroot/iosxapp.com/data/udidcert/${record.udid}/${record.udid}.mobileprovision`;
-    // // 判断这个文件是否存在
-    // if (!fs.existsSync(file_path)) {
-
-    //   //文件不存在做预警
-    //   // return { code: -1, message: '文件不存在' };
-
-
-
-
-
-
-    // }
-
-    // parse(file_path)
-    // .then(info => {
-    // //   console.log('App ID:', info.Entitlements['application-identifier']);
-    // //   console.log('Team ID:', info.TeamIdentifier);
-    // //   console.log('UUID:', info.UUID);
-    // //   console.log('Name:', info.Name);
-    // //   console.log('Creation Date:', info.CreationDate);
-    // //   console.log('Expiration Date:', info.ExpirationDate);
-    // //   console.log('All info:', info);
-      
-    // })
-    // .catch(err => {
-    //   console.error('Error parsing mobileprovision file:', err);
-    // });
   
   }
 
   
-  async udid_warning(id: number): Promise<any> {
+  async udid_check(udid: string): Promise<any> {
   
- 
-    //延迟5s后重试
-    await new Promise(resolve => setTimeout(resolve, 50000));
-    const record = await this.superUDIDRepository.findOne({
-      where: { id: id },
-    });
 
-    if(isEmpty(record.cert_iss)){
 
-      if(!this.udids.includes(record.udid)){
-        this.udids.push(record.udid);
-        this.logService.warning(record.udid);
-      }
+    let dircetory = `/www/wwwroot/iosxapp.com/data/udidcert/${udid}`;
 
-  
-      //警告
-      // return { code: -1, message: '未找到对应数据' };
+
+    // 获取dircetory下后缀为mobileprovision的文件
+    let mobileprovisionFiles: string[] = [];
+    if (fs.existsSync(dircetory) && fs.statSync(dircetory).isDirectory()) {
+      mobileprovisionFiles = fs.readdirSync(dircetory)
+        .filter(file => file.endsWith('.mobileprovision'))
+        .map(file => `${dircetory}/${file}`);
+
+        let mobileprovisionFile = mobileprovisionFiles[0];
+
+        console.log(mobileprovisionFile);
+
+        let mobileprovision = provisioning.parse(mobileprovisionFile);
+
+        let uuid = mobileprovision.UUID;
+        console.log(uuid);
+
+
+
+    }else{
+
+
     }
+
+ 
+    // //延迟5s后重试
+    // // await new Promise(resolve => setTimeout(resolve, 5000));
+    // const record = await this.superUDIDRepository.findOne({
+    //   where: { id: id },
+    // });
+
+    // if(isEmpty(record.cert_iss)){
+
+    //   if(!this.udids.includes(record.udid)){
+    //     this.udids.push(record.udid);
+    //     this.logService.warning(record.udid);
+    //   }
+
+  
+    //   //警告
+    //   // return { code: -1, message: '未找到对应数据' };
+    // }
 
   
   }
