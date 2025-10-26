030c62baeebed534d333b580156b22d80a908c4a
nidom <nidom@qq.com>
2025-10-27 00:34:01 +0800

    ..

--- src/domain_api/domain.module.ts
+++ src/domain_api/domain.module.ts
@@ -6,8 +6,9 @@ https://docs.nestjs.com/modules
 
 import { Module } from '@nestjs/common';
 import { TypeModule } from 'src/entitis/typeModule';
+import { ActionModule } from 'src/actions/action.module';
 @Module({
-    imports: [TypeModule],
+    imports: [TypeModule,ActionModule],
     controllers: [
         DomainController,],
     providers: [
