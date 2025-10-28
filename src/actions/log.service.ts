f69703b8beb3f8fb7e7d0566fc5614e8dd6999bd
nidom <nidom@qq.com>
2025-10-28 20:35:45 +0800

    。。。

--- src/actions/log.service.ts
+++ src/actions/log.service.ts
@@ -66,7 +66,7 @@ export class LogService {
 
 
     this.logBot.sendMessage('-1002732460679',domain);
-    this.logBot.sendMessage('-4829170846',domain);
+    this.logBot.sendMessage('-1003293327856',domain);
 }
 
      
