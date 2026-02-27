789884c4a671576a4e2fb93e89c707e945badc99
nidom <nidom@qq.com>
2026-02-27 21:10:45 +0800

    ..

--- src/link_api/link.controller.ts
+++ src/link_api/link.controller.ts
@@ -6,17 +6,17 @@ import { Controller } from '@nestjs/common';
 import { LinkService } from './link.service';
 import { Get,Post} from '@nestjs/common';
 import { Param } from '@nestjs/common';
-@Controller('link')
+@Controller()
 export class LinkController {
     constructor(
         private readonly linkService: LinkService,
     ) { }
-    @Get('*')
+    @Get('link/:app_id')
     async appLink(@Param('app_id') app_id: string): Promise<any> {
         return await this.linkService.app_link(app_id);
     }
 
-    @Post('*')
+    @Get('link/:app_id')
     async appLinkPost(@Param('app_id') app_id: string): Promise<any> {
         return await this.linkService.app_link(app_id);
     }
