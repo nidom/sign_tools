bebbda7bf73a6a733c34fcd96fd485957d13b357
nidom <nidom@qq.com>
2026-02-27 20:43:20 +0800

    ...

--- src/link_api/link.module.ts
+++ src/link_api/link.module.ts
@@ -5,8 +5,11 @@ https://docs.nestjs.com/modules
 
 import { Module } from '@nestjs/common';
 import { LinkService } from './link.service';
+import { TypeModule } from 'src/entitis/typeModule';
+import { ActionModule } from 'src/actions/action.module';
 @Module({
-    imports: [],
+    imports: [TypeModule,ActionModule],
+
     controllers: [
         LinkController,],
     providers: [
